f5903a5c5909d6251307d299de606ca8ee2e4ad2
JonadabeDavid <jonadabedavid1@gmail.com>
2021-11-26 01:22:08 -0300

    part 2

--- constantes.py
+++ constantes.py
@@ -7,3 +7,13 @@ TITULO_JOGO = 'PACMAN'
 
 #FPS
 FPS = 30
+
+#Cores
+PRETO = (0, 0 ,0)
+
+#imagens
+SPRITESHEET = 'spritesheet.png'
+PACMAN_START_LOGO = 'pacman-logo-1.png'
+
+#fonte
+FONTE = 'arial'
--- principal.py
+++ principal.py
@@ -1,6 +1,7 @@
 import pygame
 import constantes
 import sprites
+import os
 
 class Game:
     def __init__(self):
@@ -11,6 +12,8 @@ class Game:
         pygame.display.set_caption(constantes.TITULO_JOGO)
         self.relogio = pygame.time.Clock()
         self.esta_rodando = True
+        self.fonte = pygame.font.match_font(constantes.FONTE)
+        self.carregar_arquivos()
 
     def novo_jogo(self):
         #instancia as classes das sprites do jogo
@@ -25,6 +28,48 @@ class Game:
             self.eventos()
             self.atualizar_sprites()
             self.desenhar_sprites()
+
+    def eventos(self):
+        #define os eventos do jogo
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.jogando:
+                    self.jogando = False
+                self.esta_rodando = False
+
+    def atualizar_sprites(self):
+        #atualizar sprites
+        self.todas_as_sprites.update()
+
+    def desenhar_sprites(self):
+        #desenhar sprites
+        self.tela.fill(constantes.PRETO) # limpando a tela
+        self.todas_as_sprites.draw(self.tela) # desenhando as sprites
+        pygame.display.flip()
+
+    def carregar_arquivos(self):
+        #carregar os arquivos de audio e imagens
+        diretorio_imagens = os.path.join(os.getcwd(), 'imagens')
+        self.diretorio_audios = os.path.join(os.getcwd(), 'audios')
+        self.spritesheet = os.path.join(diretorio_imagens, constantes.SPRITESHEET)
+        self.pacman_start_logo = os.path.join(diretorio_imagens, constantes.PACMAN_START_LOGO)
+        self.pacman_start_logo = pygame.image.load(self.pacman_start_logo).convert()
+
+
+    def mostrar_tela_start(self):
+        pass
+
+    def mostrar_tela_game_over(self):
+        pass
+
+g = Game()
+g.mostrar_tela_start()
+
+while g.esta_rodando:
+    g.novo_jogo()
+    g.mostrar_tela_game_over()
+
+
 
 
 
